984e4096e5d5b1307da4f5c6f93b6d501061204f
sabin411 <sherasabin411@gmail.com>
2023-05-08 10:12:53 +0545

    ✨Feat:Worked with literal-types

---
+++ src/global/literal-types.ts
@@ -0,0 +1,11 @@
+export const age = 31;
+export const name = "sabin";
+
+const tsPeople = ["Sabin", "Aarzu", "Gaurab", "Babin"] as const;
+
+const moreTsProple = {
+  Sabin: "Sabin",
+  Aarzu: "Aarzu",
+  Gaurab: "Gaurab",
+  Babin: "Babin",
+} as const;
